652a49955ca5e72b2f1ad1e834984ebf7e099406
root <root@ip-172-31-89-173.ec2.internal>
2018-02-07 15:59:12 +0000

    DFDFSA

--- brigade.js
+++ brigade.js
@@ -7,7 +7,7 @@ events.on("push", function(e, project) {
   var node = new Job("test-runner")
 
   // We want our job to run the stock Docker Python 3 image
-  node.image = "ubuntu:16.04"
+  node.image = "sugendh_brigade:1.2"
 
   // Now we want it to run these commands in order:
   node.tasks = [
