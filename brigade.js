b682d47ee3bc829fe359342e3696a969773c6405
root <root@ip-172-31-89-173.ec2.internal>
2018-02-07 19:17:44 +0000

    fdfd

--- brigade.js
+++ brigade.js
@@ -14,7 +14,7 @@ events.on("push", function(e, project) {
 //    "echo Welcome to Brigade, Next Big Thing!"
 //    "helm install --namespace wordpress --name wordpress --set serviceType=NodePort stable/wordpress"
 //    `kubectl get pods`
-     `helm install --namespace wordpress --name wordpress --set serviceType=LoadBalancer /opt/stable/wordpress`
+     `cd /opt && helm install --namespace wordpress --name wordpress --set serviceType=LoadBalancer stable/wordpress`
 //    `helm install --namespace wordpress --name wordpress --set serviceType=LoadBalancer stable/wordpress`
   ]
 
